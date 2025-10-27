c7036d70e783fe851fb1d08d64e4173e548b3105
PavelVaavra <pavelvavra.vm@gmail.com>
2025-10-27 15:49:41 +0100

    Input and Verbose

--- main.py
+++ main.py
@@ -1,20 +1,34 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 def main():
+    if len(sys.argv) < 2:
+        sys.exit("You have to provide a prompt.")
+    user_prompt = sys.argv[1]
+
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
+
+    from google.genai import types
+
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
 
     client = genai.Client(api_key=api_key)
     response = client.models.generate_content(
         model='gemini-2.0-flash-001', 
-        contents='Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.'
+        contents=messages
     )
     print(response.text)
 
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    if "--verbose" in sys.argv: 
+        print(f"User prompt: {user_prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
 
 if __name__ == "__main__":
