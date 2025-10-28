3965548a911ae302a293a9d14a48411a63ac2c52
PavelVaavra <pavelvavra.vm@gmail.com>
2025-10-28 21:02:21 +0100

    Get File Content

--- tests.py
+++ tests.py
@@ -1,22 +1,23 @@
 # tests.py
-from functions.get_files_info import get_files_info
+from functions.get_file_content import get_file_content
 
 def test():
-    print("Result for current directory:")
-    print(get_files_info("calculator", "."))
-    print("")
+    print("=====Content of main.py=====")
+    print(get_file_content("calculator", "main.py"))
+    print("==========\n")
 
-    print("Result for 'pkg' directory:")
-    print(get_files_info("calculator", "pkg"))
-    print("")
+    print("=====Content of pkg/calculator.py=====")
+    print(get_file_content("calculator", "pkg/calculator.py"))
+    print("==========\n")
 
-    print("Result for '/bin' directory:")
-    print(get_files_info("calculator", "/bin"))
-    print("")
-    
-    print("Result for '../' directory:")
-    print(get_files_info("calculator", "../"))
-    print("")
+    print("=====Content of /bin/cat=====")
+    print(get_file_content("calculator", "/bin/cat"))
+    print("==========\n")
+
+    print("=====Content of pkg/does_not_exist.py=====")
+    print(get_file_content("calculator", "pkg/does_not_exist.py"))
+    print("==========\n")
+
 
 
 if __name__ == "__main__":
